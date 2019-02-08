bf3e0b213e82eb33475f78d82466e525a7ca6802
ChayaSt <chaya.stern@choderalab.org>
2019-02-08 16:24:23 -0500

    fix to support multiple conformer initial molecule in fragmenter

--- cmiles/_cmiles_oe.py
+++ cmiles/_cmiles_oe.py
@@ -247,8 +247,9 @@ def get_map_ordered_geometry(molecule, atom_map):
     if not molecule.GetDimension() == 3:
         raise RuntimeError("Molecule must have 3D coordinates for generating a QCSchema molecule")
 
-    if molecule.GetMaxConfIdx() != 1:
-        raise Warning("The molecule must have at least and at most 1 conformation")
+    if isinstance(molecule, oechem.OEMol):
+        if molecule.GetMaxConfIdx() != 1:
+            raise Warning("The molecule must have at least and at most 1 conformation")
 
     symbols = []
     geometry = []
